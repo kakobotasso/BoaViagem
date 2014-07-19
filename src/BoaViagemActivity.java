35ddd02e0d378f542376fd2ef9dac1ee9074c9d5
Kako Botasso <kakodev@gmail.com>
2014-07-19 14:17:43 -0300

    Criando layout da tela de login

---
+++ src/BoaViagemActivity.java
@@ -0,0 +1,15 @@
+import br.com.kakobotasso.boaviagem.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class BoaViagemActivity extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+	}
+	
+	public void entrarOnClick(View v){}
+}
