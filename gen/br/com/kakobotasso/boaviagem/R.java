91773fa24a461b971a2e66cc0d46bfbb70841f34
Kako Botasso <kakodev@gmail.com>
2014-07-19 19:37:08 -0300

    Implementando activity de login e layout do dashboard

--- gen/br/com/kakobotasso/boaviagem/R.java
+++ gen/br/com/kakobotasso/boaviagem/R.java
@@ -21,15 +21,28 @@ public final class R {
         public static final int novo_gasto=0x7f020007;
     }
     public static final class id {
-        public static final int senha=0x7f060001;
-        public static final int usuario=0x7f060000;
+        public static final int configuracoes=0x7f060003;
+        public static final int minhas_viagens=0x7f060002;
+        public static final int nova_viagem=0x7f060001;
+        public static final int novo_gasto=0x7f060000;
+        public static final int senha=0x7f060005;
+        public static final int usuario=0x7f060004;
     }
     public static final class layout {
-        public static final int login=0x7f030000;
+        public static final int dashboard=0x7f030000;
+        public static final int login=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int configuracoes=0x7f040008;
         public static final int entrar=0x7f040003;
+        public static final int erro_autenticacao=0x7f040004;
+        public static final int minhas_viagens=0x7f040007;
+        public static final int nova_viagem=0x7f040006;
+        /**  / TELA DE LOGIN 
+ TELA DE DASHBOARD 
+         */
+        public static final int novo_gasto=0x7f040005;
         public static final int senha=0x7f040002;
         /**  TELA DE LOGIN 
          */
